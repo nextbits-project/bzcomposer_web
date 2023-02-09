256657c688da8501610eba05a0d592d4fc19f6db
Kamran Omar <kamiomar@gmail.com>
2023-02-09 13:33:05 +0500

    implemnt new theme.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,5 +1,7 @@
 package com;
+
 import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -15,7 +17,6 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer implements WebMvcConfigurer {
 
@@ -24,46 +25,45 @@ public class Application extends SpringBootServletInitializer implements WebMvcC
 	}
 
 	@Bean
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
 	@Bean("messageSource")
 	public ReloadableResourceBundleMessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasenames("/WEB-INF/locale/messages");
-	    messageSource.setCacheSeconds(3600);
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
+		messageSource.setBasenames("/WEB-INF/locale/messages");
+		messageSource.setCacheSeconds(3600);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 
 	@Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        registry.viewResolver(resolver);
-    }
-	
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		registry.viewResolver(resolver);
+	}
+
 	@Bean
-	   public LocaleResolver localeResolver() {
-	      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-	      sessionLocaleResolver.setDefaultLocale(Locale.US);
-	      return sessionLocaleResolver;
-	 }
-	
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-	    localeChangeInterceptor.setParamName("lang");
-	    return localeChangeInterceptor;
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 
-	
 }
